bed75542f9794de81a5c4f91f36065b6bac92ddb
Jonas <alessi.jonas@gmail.com>
2022-10-04 21:57:14 +0300

    Create initial domain logic

--- broker-service/domain/src/main/kotlin/org/broker/domain/order/entity/Order.kt
+++ broker-service/domain/src/main/kotlin/org/broker/domain/order/entity/Order.kt
@@ -1,11 +1,53 @@
 package org.broker.domain.order.entity
 
+import org.broker.domain.order.exception.MarketClosedException
+import org.broker.domain.order.exception.OrderMinimumException
+import org.broker.domain.order.exception.WithoutBalanceException
 import org.shared.domain.entity.AggregateRoot
+import org.shared.domain.entity.Share
+import org.shared.domain.entity.ShareCategory
 import org.shared.domain.vo.OrderId
+import java.math.BigDecimal
+import java.math.RoundingMode
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.util.*
 
-class Order private constructor(override val id: OrderId) : AggregateRoot<OrderId>() {
+class Order private constructor(override val id: OrderId, val balance: BigDecimal) : AggregateRoot<OrderId>() {
+    private var total: BigDecimal? = null
+    private var share: Share? = null
+    private var quantity: Int = 0
+
+    fun buy(share: Share, quantity: Int) {
+        validateQuantity(share, quantity)
+        val totalWithoutFee = share.price.multiply(quantity.toBigDecimal())
+        val amount = totalWithoutFee.add(totalWithoutFee.multiply(FEE)).setScale(2, RoundingMode.CEILING)
+        if (amount > balance) throw WithoutBalanceException(amount)
+        this.total = amount
+        this.share = share
+        this.quantity = quantity
+    }
+
+    private fun validateQuantity(share: Share, quantity: Int) {
+        if (share.category == ShareCategory.FRACTIONAL && quantity < 1) {
+            throw OrderMinimumException("$quantity is not acceptable for fractional shares")
+        }
+        if (share.category == ShareCategory.INTEGRAL && quantity % 100 != 0) {
+            throw OrderMinimumException("$quantity is not acceptable for integral shares")
+        }
+    }
 
     companion object {
-        fun create()
+        private val FEE = BigDecimal(0.02)
+        fun create(
+            balance: BigDecimal,
+            now: ZonedDateTime = ZonedDateTime.now(ZoneId.of("America/Sao_Paulo"))
+        ): Order {
+            if (now.hour * 100 + now.minute !in 1000..1700) throw MarketClosedException()
+            return Order(
+                id = OrderId(UUID.randomUUID()),
+                balance = balance
+            )
+        }
     }
 }
--- shared/domain/src/main/kotlin/org/shared/domain/entity/Share.kt
+++ shared/domain/src/main/kotlin/org/shared/domain/entity/Share.kt
@@ -1,4 +1,10 @@
 package org.shared.domain.entity
 
-class Share {
+import java.math.BigDecimal
+
+class Share(override val id: String, val price: BigDecimal, val category: ShareCategory) : Entity<String>()
+
+enum class ShareCategory{
+    FRACTIONAL,
+    INTEGRAL
 }
